70e06d520368f65e58b18660b7be02be3bf9093e
Nicholas Nassar <niccholaspage@gmail.com>
2020-03-24 13:45:33 -0400

    Start AVL header file

--- Lab 3/AVL.h
+++ Lab 3/AVL.h
@@ -1,1 +1,16 @@
 #pragma once
+
+class AVL
+{
+public:
+	AVL();
+	~AVL();
+private:
+	struct node
+	{
+		char word[50] = {};
+		int count = 1;
+		node* leftChild = nullptr;
+		node* rightChild = nullptr;
+	};
+};

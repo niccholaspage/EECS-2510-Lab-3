65da5d5fed80f1353dc0ab92538aa3fb44af0d8d
Nicholas Nassar <niccholaspage@gmail.com>
2020-03-26 10:31:12 -0400

    Run tests from main and open the correct Shakespeare file

--- Lab 3/Main.cpp
+++ Lab 3/Main.cpp
@@ -23,7 +23,7 @@ void runTests()
 
 	for (int pass = 0; pass < 6; pass++) {
 		bool IsDelimiter = false, WasDelimiter = false;
-		inFile.open("I:\\Shakespeare.txt", ios::binary);
+		inFile.open("C:\\Users\\nicch\\OneDrive\\Documents\\College\\Spring 2020\\EECS 2510\\Lab 2\\Sample Files\\Shakespeare.txt", ios::binary);
 		if (inFile.fail())
 		{
 			cout << "Unable to open input file\n\n" << "Program Exiting\n\nPress ENTER to exit\n";
@@ -64,62 +64,7 @@ void runTests()
 
 int main()
 {
-	cout << "Hello there!\n";
-
-	AVL* tree = new AVL();
-
-	tree->insert("mar");
-	tree->insert("may");
-	tree->insert("nov");
-	tree->insert("aug");
-	tree->insert("apr");
-	tree->insert("jan");
-	tree->insert("dec");
-	tree->insert("jul");
-	tree->insert("feb");
-	tree->insert("jun");
-	tree->insert("oct");
-	tree->insert("sep");
-
-	/*tree->insert("b");
-	tree->insert("a");
-	tree->insert("c");
-	tree->insert("d");
-	tree->insert("e");
-	tree->insert("f");
-	tree->insert("f");
-	tree->insert("q");
-	tree->insert("z");
-	tree->insert("k");
-	tree->insert("k");
-	tree->insert("k");
-	tree->insert("v");
-	tree->insert("z");
-	tree->insert("u");
-	tree->insert("t");
-	tree->insert("h");
-	tree->insert("s");
-	tree->insert("y");
-	tree->insert("x");
-	tree->insert("w");
-	tree->insert("m");
-	tree->insert("p");
-	tree->insert("n");
-	tree->insert("l");
-	tree->insert("o");
-	tree->insert("z");
-	tree->insert("j");
-	tree->insert("s");
-	tree->insert("i");
-	tree->insert("r");
-	tree->insert("o");
-	tree->insert("g");*/
-
-	tree->list();
-
-	tree->print2D();
-
-	delete tree;
+	runTests();
 
 	return 0;
 }

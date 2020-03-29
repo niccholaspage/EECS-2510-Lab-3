88e533861260c89a4f476170231c510b5cc51b32
niccholaspage <niccholaspage@gmail.com>
2020-03-28 20:07:42 -0400

    Add comments to RBT header file

--- Lab 3/RBT.h
+++ Lab 3/RBT.h
@@ -29,37 +29,54 @@ public:
 private:
 	struct node
 	{
-		char word[50] = {};
-		unsigned int count = 1;
-		bool color = RED; // true is black, false is red
-		node* parent = nullptr;
-		node* leftChild = nullptr;
-		node* rightChild = nullptr;
+		char word[50] = {};			// The word of the node
+		unsigned int count = 1;		// The amount of times the word has been inserted into the binary search tree
+		bool color = RED;			// The color of the node. Since we only have two possible colors, red or black, it is just a bool.
+		node* parent = nullptr;		// A pointer to the parent of the node
+		node* leftChild = nullptr;	// A pointer to the left child of the node
+		node* rightChild = nullptr;	// A pointer to the right child of the node
 	};
 
-	const static bool BLACK = true;
+	// As seen from our node struct above, the node color is a boolean, so we just make RED false and BLACK true.
+	// We will always refer to these colors with these constant static RED and BLACK variables instead of true or false.
 	const static bool RED = false;
+	const static bool BLACK = true;
 
 	void traverseDestruct(node* p); // Traverses through the given node and deletes its children recursively as well as itself
+
+	// Calculates the number of words and unique words in the skip list.
 	void calculateNumWords(unsigned int& numWords, unsigned int& numUniqueWords);
+
+	// Recursively calculates the number of words and unique words by counting the given node's words and its two children.
 	void calculateNumWords(node* p, unsigned int& numWords, unsigned int& numUniqueWords);
-	void list(int& index, node* p); // Traverses through the children of a node as well as the node and prints
-	void leftRotate(node* p);
-	void rightRotate(node* p);
-	void insertFixup(node* p);
-	unsigned int getHeight();
+
+	void list(int& index, node* p);	// Traverses through the children of a node as well as the node and prints
+	void leftRotate(node* p);		// Performs a left rotation on the given node
+	void rightRotate(node* p);		// Performs a right rotation on the given node
+	void insertFixup(node* p);		// Fixes up the insertion into the tree of the given node by performing rotations and recoloring nodes as needed.
+	unsigned int getHeight();		// Returns the height of the tree
+
+	// Recursively calculates the height of the tree by traversing through the given node and its children
 	void calculateHeight(node* p, unsigned int& height, unsigned int traversalHeight);
 
-	node* root = nullptr; // A pointer to the root of the binary search tree. If this is nullptr, no nodes exist in the tree.
+	// A pointer to the root of the binary search tree. While this is first set to nullptr,
+	// it will point to the nil node pointer below after the constructor has been called.
+	// If this points to the same node that nil points to, no nodes exist in the tree.
+	node* root = nullptr;
+
+	// A pointer to a nil node that we will set up in the constructor. nil is a special node,
+	// who's parent, left child, and right child are all nil. It is used instead of nullptr
+	// for "null" root, left child, and right child nodes to make rotations and recoloring
+	// easier to implement.
 	node* nil = nullptr;
 
-	unsigned long long numberOfRecolorings = 0;
-	unsigned long long numberOfReferenceChanges = 0;
-	unsigned long long numberOfKeyComparisonsMade = 0;
-	unsigned long long numberOfNoFixesNeeded = 0;
-	unsigned long long numberOfCase1Fixes = 0;
-	unsigned long long numberOfCase2Fixes = 0;
-	unsigned long long numberOfCase3Fixes = 0;
+	unsigned long long numberOfRecolorings = 0;			// Keeps track of the number of times a node gets recolored.
+	unsigned long long numberOfReferenceChanges = 0;	// Keeps track of the number of times the root pointer or child pointers change.
+	unsigned long long numberOfKeyComparisonsMade = 0;	// Keeps track of the number of word key comparisons during tree inserts.
+	unsigned long long numberOfNoFixesNeeded = 0;		// Keeps track of the number of times a new node is inserted without a fix performed on the tree.
+	unsigned long long numberOfCase1Fixes = 0;			// Keeps track of the number of times a case 1 fix is performed on new node insertion.
+	unsigned long long numberOfCase2Fixes = 0;			// Keeps track of the number of times a case 2 fix is performed on new node insertion.
+	unsigned long long numberOfCase3Fixes = 0;			// Keeps track of the number of times a case 3 fix is performed on new node insertion.
 
-	clock_t startTime;
+	clock_t startTime; // A variable that represents the time at very beginning of tree construction
 };
